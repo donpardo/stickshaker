edadce617a386300a52ddb1d19225cd0c340e66f
Matt <mhanes@math.gatech.edu>
2018-02-26 11:36:59 -0500

    Add TODOs and work vs home variables

--- stickshaker.py
+++ stickshaker.py
@@ -4,27 +4,33 @@ import os
 import re # regular expressions
 import shutil
 
-# my_subs should be put in a file later so it can be 
+# my_style should be put in a file later so it can be 
 # set by the user
-my_subs = { " the ":" The ", " and ":" and ", " & ":" and ", "_":" " }
+my_style = { " the ":" The ", " and ":" and ", " & ":" and ", "_":" " }
 
-DATA_ROOT = '/Users/matt/Stickshaker/data'
+# DATA_ROOT = '/Users/matt/Stickshaker/data'
+DATA_ROOT = '/nethome/mhanes/Personal/Stickshaker/data'
+# MUSIC_ROOT = '/Users/matt/Stickshaker/music_test/music_dirs'
+MUSIC_ROOT = '/nethome/mhanes/Personal/Stickshaker/music_test/music_dirs'
 DATA_FILE = 'moves'
-MUSIC_ROOT = '/Users/matt/Stickshaker/music_test/music_dirs'
-
-def my_SetStyle(CHNG_LOC):
-    # This funtion cleans up and standardizes the name of the 
-    # destination folder (DEST_LOC), using the values set in 
-    # my_subs above.
-    for my_target in my_subs:
-        my_replacement = re.compile(re.escape(my_target), re.IGNORECASE)
-        CHNG_LOC = my_replacement.sub(my_subs[my_target], CHNG_LOC)
-        CHNG_LOC = re.sub(' +', ' ', CHNG_LOC)
-    return CHNG_LOC
-
 FULL_PATH = os.path.join(DATA_ROOT,DATA_FILE)
 EXCEPT_DIR = os.path.join(MUSIC_ROOT,"EXCEPTIONS")
 CHECKME = os.path.join(MUSIC_ROOT,"CHECKME")
+
+# check that MUSIC_ROOT exists and is a directory
+# check that DATA_FILE exists
+# check that all folders on left in moves file exist
+# allow wildcards on left side of moves (example: Bob Dylan*:::Bob Dylan)
+
+def my_SetStyle(CHNG_LOC):
+    # This function cleans up and standardizes the name of the 
+    # destination folder (DEST_LOC), using the values set in 
+    # my_style above.
+    for my_target in my_style:
+        my_replacement = re.compile(re.escape(my_target), re.IGNORECASE)
+        CHNG_LOC = my_replacement.sub(my_style[my_target], CHNG_LOC)
+        CHNG_LOC = re.sub(' +', ' ', CHNG_LOC)
+    return CHNG_LOC
 
 with open(FULL_PATH, "r") as MOVE_LINE:
     for LINE in MOVE_LINE:
@@ -32,8 +38,8 @@ with open(FULL_PATH, "r") as MOVE_LINE:
         ORIG_LOC, DEST_LOC = LINE.split(":::")
         # remove trailing whitespace
         ORIG_LOC = ORIG_LOC.rstrip()
-        # OS_ORIG is the old school original directory, used below
-        OS_ORIG_LOC=ORIG_LOC
+        # OG_ORIG is the OG original directory, needed below
+        OG_ORIG_LOC=ORIG_LOC
         ORIG_LOC = os.path.join(MUSIC_ROOT,ORIG_LOC)
         DEST_LOC = DEST_LOC.rstrip()
         print("Original location = " + ORIG_LOC)
@@ -48,7 +54,7 @@ with open(FULL_PATH, "r") as MOVE_LINE:
         # If the destination has a trailing slash, the original
         # directory gets put under the destination
         if my_LastChar == '/':
-            DEST_LOC = os.path.join(DEST_LOC,OS_ORIG_LOC)
+            DEST_LOC = os.path.join(DEST_LOC,OG_ORIG_LOC)
 
         # Make the destination directory 
         if not os.path.isdir(DEST_LOC):
